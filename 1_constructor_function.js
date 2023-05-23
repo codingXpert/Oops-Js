9fd074bbaec23353cf797f0d882ecbc1e50bcf91
vivek <vivek.raj@brainvire.com>
2023-05-23 11:49:38 +0530

    functions inside a constructor function

--- 1_constructor_function.js
+++ 1_constructor_function.js
@@ -2,11 +2,21 @@ function BankAccount (customerName , balance = 0){
           this.customerName = customerName;
           this.accountNumber = Date.now();
           this.balance = balance;
+
+          this.deposite = function(amount){
+                    this.balance += amount;
+          }
+
+          this.withdraw = (amount) => {
+                    this.balance -= amount;
+          }
 }
 
 const rakeshAccount = new BankAccount('Rakesh K' , 1000);
 const johnAccount = new BankAccount('John Doe');
+rakeshAccount.deposite(5000);
+johnAccount.deposite(1000);
+rakeshAccount.withdraw(2000);
+johnAccount.withdraw(200);
 
-rakeshAccount.balance = 5000;
-console.log(rakeshAccount.balance);
 console.log(rakeshAccount , johnAccount);
